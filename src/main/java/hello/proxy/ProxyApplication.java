6cda8f3045b21754a374c453e6267ddc1c6653ad
박세준 <sjpark@selab.co.kr>
2022-11-18 13:53:23 +0900

    Class기반 Proxy 프로젝트 적용 상속을 활용하여 단점이 있음

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,6 +1,7 @@
 package hello.proxy;
 
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
@@ -9,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import({InterfaceProxyConfig.class, AppV2Config.class})
+@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
