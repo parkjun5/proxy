e6e5451258b4a6e544214906d462d6520a68e802
박세준 <sjpark@selab.co.kr>
2022-11-18 15:06:57 +0900

    JDK 동적 프록시 예제 작성 프록시를 클래스, 인터페이스마다 생성해야하는 문제를 해결

---
+++ src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
@@ -0,0 +1,53 @@
+package hello.proxy.jdkdynamic;
+
+import hello.proxy.jdkdynamic.code.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Proxy;
+
+@Slf4j
+public class JdkDynamicProxyTest {
+
+    @Test
+    void dynamicA() {
+        AInterface target = new AInterfaceImpl();
+        TimeInvocationHandler handler = new TimeInvocationHandler(target);
+        AInterface proxy = (AInterface) Proxy.newProxyInstance(AInterface.class.getClassLoader(), new Class[]{AInterface.class}, handler);
+
+        proxy.call();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+    }
+
+    @Test
+    void dynamicB() {
+        BInterface target = new BInterfaceImpl();
+        TimeInvocationHandler handler = new TimeInvocationHandler(target);
+        BInterface proxy = (BInterface) Proxy.newProxyInstance(BInterface.class.getClassLoader(), new Class[]{BInterface.class}, handler);
+
+        proxy.call();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+    }
+
+    @Test
+    void dynamicDouble() {
+        AInterface target = new AInterfaceImpl();
+        TimeInvocationHandler handler = new TimeInvocationHandler(target);
+        AInterface proxy = (AInterface) Proxy.newProxyInstance(AInterface.class.getClassLoader(), new Class[]{AInterface.class}, handler);
+
+        proxy.call();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        BInterface target2 = new BInterfaceImpl();
+        TimeInvocationHandler handler2 = new TimeInvocationHandler(target2);
+        BInterface proxy2 = (BInterface) Proxy.newProxyInstance(BInterface.class.getClassLoader(), new Class[]{BInterface.class}, handler2);
+
+        proxy2.call();
+        log.info("targetClass={}", target2.getClass());
+        log.info("proxyClass={}", proxy2.getClass());
+    }
+
+}
