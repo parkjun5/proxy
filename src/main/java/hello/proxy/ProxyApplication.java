1a62330b857b4eb5609fbf916346f29fed7371d4
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2022-11-23 21:30:46 +0900

    스프링이 제공하는 빈 후처리기 적용

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,8 +1,12 @@
 package hello.proxy;
 
 
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -10,7 +14,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import({ProxyFactoryConfigV1.class, ProxyFactoryConfigV2.class})
+//@Import({ProxyFactoryConfigV1.class, ProxyFactoryConfigV2.class})
+//@Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
